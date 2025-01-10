0e49556d0b98cf91da7d432d5fb0ab052de32a09
ahmed <bintzonedata@gmail.com>
2025-01-10 18:00:25 +0300

    lopop;

--- app/forms/LoginForm.tsx
+++ app/forms/LoginForm.tsx
@@ -1,10 +1,11 @@
-'use client';
-import { UserLoginSchema } from '@/lib/types/auth';
-import { singInValidation } from '@/lib/validations/auth';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import PhoneInput from '@/components/shared/PhoneInput';
-import { Button } from '@/components/ui/button';
+"use client";
+import { UserLoginSchema } from "@/lib/types/auth";
+import { singInValidation } from "@/lib/validations/auth";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import PhoneInput from "@/components/shared/PhoneInput";
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
 function LoginForm() {
   const {
     register,
@@ -16,20 +17,25 @@ function LoginForm() {
   } = useForm<UserLoginSchema>({
     resolver: zodResolver(singInValidation),
   });
+  const phone = watch("phone");
+  const password = watch("password");
   const onSubmit = async (datas: UserLoginSchema): Promise<void> => {
-    console.log('Form submitted', datas); // Debug output
+    console.log("Form submitted"); // Debug output
   };
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
       <PhoneInput
         setValue={setValue}
         clearErrors={clearErrors}
-        name='phone'
-        label='phone'
+        name="phone"
+        label="phone"
         error={errors.phone}
-        placeholder='Enter phone number'
+        placeholder="Enter phone number"
       />
-      <button type='submit'>Submit</button>
+      <button type="submit">Submit</button>
     </form>
   );
 }
--- components/shared/PhoneInput.tsx
+++ components/shared/PhoneInput.tsx
@@ -1,53 +1,54 @@
-import React from 'react';
-import PhoneInputComponent from 'react-phone-input-2';
+import React from "react";
+import PhoneInputComponent from "react-phone-input-2";
 type PhoneInputProps = {
   setValue: any;
   name: string;
   label: string;
   error: any;
   placeholder: string;
-  clearErrors: () => void;
+  clearErrors: any;
 };
 function PhoneInput({
   setValue,
-  name = '',
+  name = "",
   label,
   error,
-  placeholder = '',
+  placeholder = "",
   clearErrors,
 }: PhoneInputProps) {
   const onPhoneChange = (value: string, data: object) => {
     const { dialCode }: { dialCode?: string } = data;
     // Ensure the dialCode starts with "+"
-    const formattedDialCode = dialCode?.startsWith('+')
+    const formattedDialCode = dialCode?.startsWith("+")
       ? dialCode
       : `+${dialCode}`;
     // Remove the dial code (including the "+" symbol) from the value
-    const phoneNumber = value?.replace(dialCode || '', '')?.trim();
+    const phoneNumber = value?.replace(dialCode || "", "")?.trim();
     // Set the values in the form
     setValue(`${name}.dialCode`, formattedDialCode); // Update dialCode field
     setValue(`${name}.phone`, phoneNumber); // Update phone field
+    clearErrors("phone");
   };
   return (
-    <main className='flex-1'>
+    <main className="flex-1">
       <div>
         <label
           htmlFor={name}
-          className='block mb-2 text-sm font-medium text-gray-900 capitalize'
+          className="block mb-2 text-sm font-medium text-gray-900 capitalize"
         >
           {label}
         </label>
         <PhoneInputComponent
           placeholder={placeholder}
-          containerClass='w-full  h-[40px] rounded-xl px-2'
-          inputClass=' h-full'
-          inputStyle={{ width: '100%', height: '100%' }}
+          containerClass="w-full  h-[40px] rounded-xl px-2"
+          inputClass=" h-full"
+          inputStyle={{ width: "100%", height: "100%" }}
           onChange={onPhoneChange} // Handle changes
         />
       </div>
       {error && (
-        <span className='mt-2 text-xs text-red-500'>
-          {error.message || 'This field is required'}
+        <span className="mt-2 text-xs text-red-500">
+          {error.message || "This field is required"}
         </span>
       )}
     </main>
